fcab351f06272dd6131d545b86f1d13ade240a63
Prabhat07 <prabhatkumar27032003@gmail.com>
2026-08-07 17:47:02 +0530

    Fix students table name in schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_08_05_115149) do
-  create_table "Students", force: :cascade do |t|
+  create_table "students", force: :cascade do |t|
     t.integer "age"
     t.datetime "created_at", null: false
     t.string "email"
