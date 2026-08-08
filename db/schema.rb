e1983707c4974ad351586264f823f4d15ae829c1
Prabhat07 <prabhatkumar27032003@gmail.com>
2026-08-08 23:08:28 +0530

    install solid Queue

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,19 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_05_115149) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "Students", force: :cascade do |t|
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.integer "mobile"
+    t.string "name"
+    t.string "password"
+    t.datetime "resets_password_sent_at"
+    t.string "resets_password_token"
+    t.string "role"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "achievements", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
