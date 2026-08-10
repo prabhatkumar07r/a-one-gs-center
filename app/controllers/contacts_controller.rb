260c6e7511f5ea430e4460a792cb19e7e4f4ceb5
Prabhat07 <prabhatkumar27032003@gmail.com>
2026-08-10 13:50:23 +0530

    updated the contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,40 +1,41 @@
 class ContactsController < ApplicationController
   def create
-    @contact = Contact.new(contact_params)
+  @contact = Contact.new(contact_params)
 
-    if @contact.save
-      begin
-        Rails.logger.info "BREVO_LOGIN=#{ENV['BREVO_LOGIN']}"
-        Rails.logger.info "BREVO_SENDER=#{ENV['BREVO_SENDER']}"
-        Rails.logger.info "BREVO_KEY_PRESENT=#{ENV['BREVO_SMTP_KEY'].present?}"
+  if @contact.save
+    begin
+      Rails.logger.info "BREVO_LOGIN=#{ENV['BREVO_LOGIN']}"
+      Rails.logger.info "BREVO_SENDER=#{ENV['BREVO_SENDER']}"
+      Rails.logger.info "BREVO_API_KEY_PRESENT=#{ENV['BREVO_API_KEY'].present?}"
 
-        response = BrevoService.send_contact(@contact)
+      response = BrevoService.send_contact(@contact)
 
-        if response.success?
-          redirect_to sample_homepage_path,
-                      notice: "Message sent successfully."
-        else
-          Rails.logger.error response.body
+      Rails.logger.info "Brevo Status: #{response.code}"
+      Rails.logger.info "Brevo Body: #{response.body}"
 
-          redirect_to sample_homepage_path,
-                      alert: "Email sending failed."
-        end
-
-      rescue => e
-        Rails.logger.error "=============================="
-        Rails.logger.error e.class
-        Rails.logger.error e.message
-        Rails.logger.error e.backtrace.join("\n")
-        Rails.logger.error "=============================="
+      if [200, 201, 202].include?(response.code.to_i)
+        redirect_to sample_homepage_path,
+                    notice: "Message sent successfully."
+      else
+        Rails.logger.error response.body
 
         redirect_to sample_homepage_path,
-                    alert: "Email failed: #{e.class}"
+                    alert: "Email sending failed."
       end
-    else
+
+    rescue => e
+      Rails.logger.error e.class
+      Rails.logger.error e.message
+      Rails.logger.error e.backtrace.join("\n")
+
       redirect_to sample_homepage_path,
-                  alert: "Message could not be sent."
+                  alert: "Email failed: #{e.message}"
     end
+  else
+    redirect_to sample_homepage_path,
+                alert: "Message could not be sent."
   end
+end
 
   private
 
