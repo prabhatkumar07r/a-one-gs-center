ae89afc25a1ba7b59abaf8efec0fbd55db83c6a7
Prabhat07 <prabhatkumar27032003@gmail.com>
2026-09-05 17:27:52 +0530

    Fix teacher playlist position management

--- app/controllers/teacher_panel/playlists_controller.rb
+++ app/controllers/teacher_panel/playlists_controller.rb
@@ -2,11 +2,6 @@ class TeacherPanel::PlaylistsController < ApplicationController
 
   before_action :authenticate_user!
   before_action :teacher_or_admin
-    before_action :set_teacher_course
-
-
-  layout "teacher"
-
   before_action :set_course
   before_action :set_playlist, only: [
     :show,
@@ -15,105 +10,91 @@ class TeacherPanel::PlaylistsController < ApplicationController
     :destroy
   ]
 
+  layout "teacher"
 
   def index
-    @playlists = @course.playlists.order(:position)
+    normalize_positions
+    @playlists = @course.playlists.order(:position, :id)
   end
-
 
   def show
   end
-
 
   def new
     @playlist = @course.playlists.new
   end
 
-
   def create
-
     @playlist = @course.playlists.new(playlist_params)
 
+    @playlist.position =
+      (@course.playlists.maximum(:position) || 0) + 1
+
     if @playlist.save
-
       redirect_to teacher_panel_course_playlists_path(@course),
-      notice: "Playlist created successfully."
-
+                  notice: "Playlist created successfully."
     else
-
-      render :new,
-      status: :unprocessable_entity
-
+      render :new, status: :unprocessable_entity
     end
-
   end
-
 
   def edit
   end
 
-
   def update
-
     if @playlist.update(playlist_params)
-
       redirect_to teacher_panel_course_playlists_path(@course),
-      notice: "Playlist updated successfully."
-
+                  notice: "Playlist updated successfully."
     else
-
-      render :edit,
-      status: :unprocessable_entity
-
+      render :edit, status: :unprocessable_entity
     end
-
   end
-
 
   def destroy
-
     @playlist.destroy
 
+    normalize_positions
+
     redirect_to teacher_panel_course_playlists_path(@course),
-    notice: "Playlist deleted successfully."
-
+                notice: "Playlist deleted successfully."
   end
-
 
   private
 
-
-  
-
+  def set_course
+    @course = Course.find(params[:course_id])
+  end
 
   def set_playlist
     @playlist = @course.playlists.find(params[:id])
   end
 
-
   def playlist_params
-
     params.require(:playlist).permit(
       :title,
-      :description,
-      :position
+      :description
     )
-
   end
 
+  def normalize_positions
+  @course.playlists
+         .order(Arel.sql("position ASC NULLS LAST"), :id)
+         .each_with_index do |playlist, index|
 
-  def teacher_or_admin
+    desired_position = index + 1
 
-    unless current_user.teacher? || current_user.admin?
-
-      redirect_to root_path,
-      alert:"Access Denied"
-
+    if playlist.position != desired_position
+      playlist.update_column(:position, desired_position)
     end
 
   end
-  def set_course
-  @course = Course.find(params[:course_id])
 end
+
+  def teacher_or_admin
+    unless current_user.teacher? || current_user.admin?
+      redirect_to root_path,
+                  alert: "Access Denied"
+    end
+  end
 
 end
