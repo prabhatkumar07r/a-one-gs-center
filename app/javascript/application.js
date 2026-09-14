ec86458a437bb460bf8157a8c35a14474220f1ca
Prabhat07 <prabhatkumar27032003@gmail.com>
2026-09-14 14:03:51 +0530

    Update the Hero UI

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,12 +3,10 @@ import "controllers"
 
 import "bootstrap"
 
-import "notification"
 import "chartkick"
 import { Chart } from "chart.js"
 
 window.Chart = Chart
 
-import "custom"
 import "header"
 import "profile"
